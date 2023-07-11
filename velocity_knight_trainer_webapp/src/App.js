db1e020b286f4927f29de8a23cef91583976a867
Jonas Lekgau <77699400+codewithwest@users.noreply.github.com>
2023-07-11 13:58:39 +0200

    hmee and auth

--- velocity_knight_trainer_webapp/src/App.js
+++ velocity_knight_trainer_webapp/src/App.js
@@ -8,28 +8,12 @@ import { CreateSession } from './templates/routes/CreateSession';
 import { AthleteProfile } from './templates/routes/AthleteProfile';
 import { SessionLog } from './templates/routes/SessionLog';
 import { Home } from './templates/Home';
-// const Home = lazy(() => import('./templates/Home'))
-const Login = lazy(() => import('./templates/auth/Login'))
-const Register = lazy(() => import('./templates/auth/Register'))
-const Profile = lazy(() => import('./templates/Profile'))
+import Register from './templates/auth/Register';
+import Login from './templates/auth/Login';
+import LandingNavBar from './templates/partials/LandingNav';
 
 
 const display_handler = new displayHandler()
-
-function navigate(_nav_link_div, _ind) {
-  let links = document.querySelectorAll(_nav_link_div)
-  let _divs = document.querySelectorAll('.body-cont')
-  let tap_divs = document.querySelectorAll('.body-cont')
-  links.forEach((element, tap_divs) => {
-    if (_ind === tap_divs) {
-      display_handler.displayFlex(_divs[tap_divs].className.split(' ')[1])
-    } else {
-      display_handler.displayNone(_divs[tap_divs].className.split(' ')[1])
-    }
-  });
-}
-
-
 function App() {
   var session_token = sessionStorage.getItem('token')
   const [initial_login_state, set_initial_login_state] = useState()
@@ -74,53 +58,48 @@ function App() {
   }
 
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/newsession" element={<CreateSession />} />
-      <Route path="/sessioncollection" element={<SessionLog />} />
-      <Route path="/profile" element={<AthleteProfile />} />
-    </Routes>
 
 
-    // <div className="main">
-    //   <button className='login-state b-none  pos-abs'
-    //     onClick={() => initial_login_state ?
-    //       display_handler.displayFlex('profile-form-cont') :
-    //       display_handler.displayFlex('login-form-cont')}>
-    //     {initial_login_state ? logged_in_icon : logged_out_icon}
-    //   </button>
-    //   <nav className='nav-cont center-content fill d-flex'>
-    //     <div className='nav d-flex text-center   m-auto-hor'>
-    //       <Link to="/newsession" className='main_nav_link d-flex radius-circle center-content'>
-    //         CREATE SESSION
-    //       </Link>
-    //       <Link to="/sessioncollection" className='main_nav_link radius-circle d-flex center-content'>
-    //         SESSIONS LOG
-    //       </Link>
-    //       <Link to="/profile" className='main_nav_link d-flex radius-circle center-content'>
-    //         PROFILE
-    //       </Link>
-    //     </div>
-    //   </nav >
-    //   <Login onChange={handleLoginState}
-    //   />
-    //   <Register />
-    //   <Profile onChange={handleLoginState}
-    //     value={initial_login_state ? login_data : ""} />
-    //   <section className='main-cont'>
-    //     {/* <h1 className='bg-success pos-abs top-0 left-0'>{`${login_state}`}</h1> */}
 
-    //     <div className='home body-cont'>
-    //       <Routes>
-    //         <Route path="/newsession" element={<CreateSession />} />
-    //         <Route path="/sessioncollection" element={<SessionLog />} />
-    //         <Route path="/profile" element={<AthleteProfile />} />
-    //       </Routes>
-    //     </div>
+    <div className="main">
+      {/* <button className='login-state b-none  pos-abs'
+        onClick={() => initial_login_state ?
+          display_handler.displayFlex('profile-form-cont') :
+          display_handler.displayFlex('login-form-cont')}>
+        {initial_login_state ? logged_in_icon : logged_out_icon}
+      </button> */}
+      {/* <nav className='nav-cont center-content fill d-flex'>
+        <div className='nav d-flex text-center   m-auto-hor'>
+          <Link to="/newsession" className='main_nav_link d-flex radius-circle center-content'>
+            CREATE SESSION
+          </Link>
+          <Link to="/sessioncollection" className='main_nav_link radius-circle d-flex center-content'>
+            SESSIONS LOG
+          </Link>
+          <Link to="/profile" className='main_nav_link d-flex radius-circle center-content'>
+            PROFILE
+          </Link>
+        </div>
+      </nav > */}
 
 
-    //   </section>
-    // </div >
+      {/* Nav Via Langing */}
+      {/* {document.location.pathname == '/' ? */}
+      {LandingNavBar()}
+      {/* : true} */}
+      <div className='home body-cont fill'>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/new/session" element={<CreateSession />} />
+          <Route path="/sessions/collection" element={<SessionLog />} />
+          <Route path="/athlete/profile" element={<AthleteProfile />} />
+          <Route path="/app/auth/user/register" element={<Register />} />
+          <Route path="/app/auth/user/login" element={<Login />} onChange={handleLoginState}
+            value={initial_login_state ? login_data : ""} />
+          <Route path="/app/auth/user/profile" element={<AthleteProfile />} />
+        </Routes>
+      </div>
+    </div>
   );
 }
 
--- velocity_knight_trainer_webapp/src/templates/Home.js
+++ velocity_knight_trainer_webapp/src/templates/Home.js
@@ -1,12 +1,85 @@
-import '../styles/Home.css'  
-const Home = () => {
-  
-    
-      <div className="west bg-fail fill">
-        <p>Profile</p>
+import "../styles/Home.css";
+import "../styles/Const.css";
+
+
+import img from "../assets/0.jpg";
+import { Link } from "react-router-dom";
+export function Home() {
+
+  function boxes() {
+    return (
+
+      <div className="who d-flex wrap">
+        <div className="img-cont flex-wrap">
+          <img src={img} alt="" className="fill" />
+          <h5 className="text-center">By WestDynamics</h5>
+        </div>
+        <div className="body flex-col h-100">
+          <h2 className="header">Header</h2>
+          <div className="content">
+            This is the Content
+            This is the Content
+            This is the Content
+            This is the Content
+            This is the Content
+          </div>
+          <div className="footer d-flex  ">
+            <Link to="/app/about" className='d-flex   link'>
+              <i class="bi bi-arrow-down-right-circle text-info" style={{ fontSize: 40 }}>
+              </i>
+            </Link>
+          </div>
+        </div>
       </div>
-    
-  
+    );
+  }
+
+  // return arr
+
+  return (
+    <div className="home-cont d-flex wrap">
+      {introCards()}
+      {/* {boxes()}
+      {boxes()}
+      {boxes()}
+      {boxes()}*/}
+      {boxes()}
+    </div>
+  );
+
+
+
+  function introCards() {
+    return (
+      <div className="intro-cont d-flex wrap j-sa w-100">
+        <div className="left-card d-flexd-flex wrap">
+          <h2>This is The design Header</h2>
+          <p>This is the lispum text theat
+            this ti sjbo mnboi djwqu wq
+            this ti sjbo mnboi djwqu wq
+            this ti sjbo mnboi djwqu wq
+          </p>
+          <Link to="/app/about" className='d-flex center-content'>
+            <p className="m-auto-vert">Find Out More</p>
+            <i class="bi bi-arrow-down-right-circle text-info">
+            </i>
+          </Link>
+
+        </div>
+        <div className="right-card d-flex wrap center-content">
+          <h2>This is The design Header</h2>
+          <p>This is the lispum text theat
+            this ti sjbo mnboi djwqu wq
+            this ti sjbo mnboi djwqu wq
+            this ti sjbo mnboi djwqu wq
+          </p>
+          <Link to="/app/about" className='d-flex center-content'>
+            <p className="m-auto-vert">See More</p>  <i class="bi bi-arrow-down-right-circle text-info">
+            </i>
+          </Link>
+        </div>
+      </div>
+    );
+  }
 }
 
-export {Home}
