11493a4d0bed6b2fa72d9fc108417969850f8393
codewithwest <jonaslekgau@gmail.com>
2024-03-24 09:53:35 +0200

    url update

--- velocity_knight_trainer_webapp/src/functions/ConstVars.js
+++ velocity_knight_trainer_webapp/src/functions/ConstVars.js
@@ -1,7 +1,13 @@
 import { main_logo } from "./imageProvider"
 
 // const resolve = 'https://node-rest-six.vercel.app'
-const resolve = 'http://192.168.202.34:3070'
+try {
+    var resolve = 'https://node-rest-six.vercel.app'
+
+} catch (error) {
+    resolve = 'http://192.168.202.34:3070'
+}
+
 const aboutCards = [
     [main_logo, 'this', 'this', 'this'],
 ]
